708803a435ec18b552f4727ffa7aa110b6d57cec
Gustavo Blanco Ticona <gustavo.blanco@tecsup.edu.pe>
2022-01-19 21:27:23 -0500

    dot env base

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,7 @@
-const BASE_URL = 'http://localhost:3008/';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({path: path.join(__dirname, '..', '.env')});
+
+const BASE_URL = process.env.BASE_URL;
 export const API_BASE_URL = `${BASE_URL}api/v1/`;
